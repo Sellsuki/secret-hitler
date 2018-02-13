6ea1d60c04cfaa4d9c2cb36de16db64626284872
aunn <aunn_it08@hotmail.com>
2018-02-13 22:44:01 +0700

    call init on created App

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,15 @@
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
-  name: 'App'
+  name: 'App',
+  methods: {
+    ...mapActions(['init'])
+  },
+  created () {
+    this.init()
+  }
 }
 </script>
 
